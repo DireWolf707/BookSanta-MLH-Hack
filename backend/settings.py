2ab2c99dd174344b2a8fd9665198055dafa2ad88
DireWolf707 <rahulsood707@gmail.com>
2021-12-26 13:52:25 +0530

    added allowed origin

--- backend/settings.py
+++ backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY",'django-insecure-f!z&s20_(3=bnfh_mzg++)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
